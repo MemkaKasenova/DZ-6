e327a3e8d5c415a4c1ee6729101290b076a679b0
Memka <mkasenova87@gmail.com>
2023-01-19 12:14:10 +0600

    дополнено

--- DZ # 6/main.swift
+++ DZ # 6/main.swift
@@ -41,30 +41,63 @@ number1 = firstNum
 number2 = secondNum
 
 
-//1 способ
+//1
 //Вызов функции calculate
-//Если значения в консоли равно nil, то вместо nil прописывается 0
+//спользуя оператор "??" сделать так чтобы если неправильно введены данные, то присваивать вместо nil число 0
+
+
 calculate(a: number1 ?? 0, b: number2 ?? 0, operation: operation)
 
-//2 способ
-//Если значение в number 1 и number 2 равно nil, то появляется сообщение об ошибке, иначе вызывается функция calculate и происходит расчет
+//2
+//Используя проверку на nil, выводить сообщение неправильно введены данные, при неправильном вводе.
+func calculate2(a: Int, b: Int, operation: String) {
+    switch operation {
+    case "+":
+        print(a + b)
+    case "-" :
+        print(a - b)
+    case "*":
+        print(a * b)
+    case "/":
+        print(a / b)
+    default:
+        ()
+    }
+}
+
 if number1 == nil || number2 == nil {
     print("Неправильно введены данные! Введите число!")
 } else {
-    calculate(a: number1!, b: number2!, operation: operation)
+    calculate2(a: number1!, b: number2!, operation: operation)
 }
 
-//3 способ
+//3
 //if-let
 //Опциональное связывание
+
+func calculate3(a: Int, b: Int, operation: String) {
+    switch operation {
+    case "+":
+        print(a + b)
+    case "-" :
+        print(a - b)
+    case "*":
+        print(a * b)
+    case "/":
+        print(a / b)
+    default:
+        ()
+    }
+}
 if let number1 = number1, let number2 = number2 {
-    calculate(a: number1, b: number2, operation: operation)
+    calculate3(a: number1, b: number2, operation: operation)
 }
 
-//Другая функция, которая принимает в себя параметры опционального типа и работает с помощью инструкции guard
 //guard:
-func calculate2(a: Int?, b: Int?, operation: String) {
-    guard let a = a, let b = b else { return }
+func calculate4(a: Int?, b: Int?, operation: String) {
+    guard let a = a, let b = b else {
+        print("Вы указали неверные данные!")
+        return }
 
     switch operation {
     case "+":
@@ -80,7 +113,7 @@ func calculate2(a: Int?, b: Int?, operation: String) {
     }
 }
 
-calculate2(a: number1, b: number2, operation: operation)
+calculate4(a: number1, b: number2, operation: operation)
 
 
 
